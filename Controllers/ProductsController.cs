0c61a1075529d4605d3f5fbf3eafc2d1055dd858
vivek <vivek@athenaeum.in>
2021-05-18 12:13:23 +0530

    controller implemented

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LT.EFCoreDemo.Services;
+using LT.EFCoreDemo.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,10 +13,44 @@ namespace LT.EFCoreDemo.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly IProductService _service;
+
+        public ProductsController(IProductService service)
+        {
+            _service = service;
+        }
+
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            return Ok(await Task.FromResult(new { Message = "Product here" }));
+            return Ok(await _service.Get());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _service.Get(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductCreateViewModel viewModel)
+        {
+            await _service.Create(viewModel);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateViewModel viewModel)
+        {
+            await _service.Update(viewModel);
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _service.Delete(id);
+            return Ok();
         }
     }
 }
